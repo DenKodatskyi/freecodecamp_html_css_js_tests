61248d49dac4ff648d9699c32edba6ca8aebcc25
Den Kodatskyi <denkodak@gmail.com>
2024-06-06 08:36:36 +0300

    Add files via upload

---
+++ JS 001 Learn Introductory JavaScript by Building a Pyramid Generator/freecodecamp_01/js/main.js
@@ -0,0 +1,11 @@
+/*  */
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test"
+console.log(secondCharacter); e.log(secondCharacter)
+
+secondCharacter = character;
+console.log(secondCharacter)
